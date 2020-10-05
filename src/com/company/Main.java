d4548f05bc343a41af73835adfb5c7c7140c7385
koebsch <koebsch@MacBook-Pro-de-admin-7.local>
2020-10-05 16:46:20 +0200

    tp 2 algo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,29 @@
 package com.company;
 
 public class Main {
+    static void score(int[] scores){
+        int max=0;
+        float total=0;
+        boolean moinsde10=false;
+        for(int i=0;i<scores.length;i++){
+            System.out.println(scores[i]);
+            total=scores[i]+total;
+            if (max<scores[i]){
+                max=scores[i];
+            }
+            if (scores[i]<10);
+            moinsde10=true;
+
+        }
+        System.out.println("contient une valeur inferieur a 10 "+moinsde10);
+        System.out.println("moyenne"+total/7);
+        System.out.println("max="+max);
+
+    }
 
     public static void main(String[] args) {
 	// write your code here
         int[] scores = new int[7];
-        float total=0;
         scores[0]=24;
         scores[1]=63;
         scores[2]=96;
@@ -13,11 +31,6 @@ public class Main {
         scores[4]=80;
         scores[5]=13;
         scores[6]=83;
-        for(int i=0;i<scores.length;i++){
-            System.out.println(scores[i]);
-            total =scores[i]+total;
-        }
-        System.out.println(total/7);
-
+        score(scores);
     }
 }
